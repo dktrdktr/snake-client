30fb1f67e5771c5ab4f2a7de7d5bdee2ef5dde68
Donatas Tubutis <dotubutis@gmail.com>
2022-01-19 19:29:53 +0000

    added name write

--- client.js
+++ client.js
@@ -10,10 +10,10 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // conn.on("connect", (message) => {
-  //   // code that does something when the connection is first established
-  //   console.log(message);
-  // });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: BUG");
+  });
 
   conn.on("data", (data) => {
     console.log(data);
