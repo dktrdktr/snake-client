f917f221c6aeff7a7208c9703d65baead3eb49a3
Donatas Tubutis <dotubutis@gmail.com>
2022-01-19 19:57:44 +0000

    completed

---
+++ input.js
@@ -0,0 +1,33 @@
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === "\u0003") {
+    console.log("Thanks for using me, ciao!");
+    process.exit();
+  }
+  switch (key) {
+    case "w":
+      connection.write("Move: up");
+      break;
+    case "a":
+      connection.write("Move: left");
+      break;
+    case "s":
+      connection.write("Move: down");
+      break;
+    case "d":
+      connection.write("Move: right");
+  }
+};
+
+module.exports = { setupInput };
